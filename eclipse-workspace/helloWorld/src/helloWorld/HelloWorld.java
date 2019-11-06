6ce4086e89cda12da95cf38ec917223b1706208f
isyviel <55379449+isyviel@users.noreply.github.com>
2019-11-06 12:31:49 +0100

    Add files via upload
    exercice seul avec essais tableaux

--- eclipse-workspace/helloWorld/src/helloWorld/HelloWorld.java
+++ eclipse-workspace/helloWorld/src/helloWorld/HelloWorld.java
@@ -15,13 +15,15 @@ public class HelloWorld {
 		
 		//ln dit va a la ligne
 		System.out.println(variable);
+		System.out.println("**********");
 		System.out.println(variable2);
+		System.out.println("**********");
 		System.out.println(variable3);
 		
 		for(int i=0;i<10;i++) {
 			System.out.println(i);
 		}
-		
+		System.out.println("**********");
 		int i = 3;
 		while(i>0) {
 			System.out.println(i);
@@ -32,5 +34,39 @@ public class HelloWorld {
 			System.out.println(i);
 			i++;
 		} while(i < 5);
+		
+		int tableau[] = {0,1,2,3,4,5,6,7,8};
+		int somme = 0;
+		String avengers[] = {"Iron Man","Captain America","Loki","Docteur Strange","Hulk"};
+		
+		System.out.println("Tableau des multiplications");
+		System.out.println("**********");
+		
+		for (int j = 0; j < tableau.length-1; j++)
+		{
+			int oldValue = tableau[j];
+			int multi = tableau[j+1] * oldValue;
+			System.out.println(multi);
+		}
+
+		System.out.println("Tableau des sommes");
+		System.out.println("**********");
+		
+		for (int j : tableau)
+		{
+			somme += j;
+			System.out.println(somme);
+		}
+		System.out.println("**********");
+		System.out.println(somme);
+		System.out.println("**********");
+		for (int k = 0; k < avengers.length; k++)
+		{
+			if (avengers[k] == "Hulk")
+			{
+				System.out.println(avengers[k]);
+			}
+		}
 	}
+	
 }
